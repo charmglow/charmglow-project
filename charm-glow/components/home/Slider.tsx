f5a417672e4a348dcd321d523dc5dd8887036a3e
charmglow <charmglowjwellery@gmail.com>
2023-10-19 22:11:31 +0500

    slider & product cards

--- charm-glow/components/home/Slider.tsx
+++ charm-glow/components/home/Slider.tsx
@@ -1,27 +1,57 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
-import { Carousel, Flex } from 'antd';
+import { Button, Carousel, Flex, Typography } from 'antd';
 
-const contentStyle: React.CSSProperties = {
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-};
+
+const { Title, Text } = Typography;
 
 const Slider: React.FC = () => (
     <Carousel autoplay>
         <div>
-            <h3 style={contentStyle}>1</h3>
+            <div className='h-[80vh] relative flex flex-start bg-red-50'>
+                <img src='/slide1.png' alt="slide1" className='h-full object-fill' />
+                <div className='absolute h-full w-full flex justify-center items-center'>
+                    <div className='h-full w-full flex flex-col '>
+                        <div className='h-full w-[90%] flex flex-col justify-center items-center max-md:justify-center max-md:items-end'>
+                            <Title level={4} className='uppercase max-md:hidden'>
+                                Elevate Style with Charm Glow
+                            </Title>
+                            <Text className='pt-3 text-base max-md:hidden' italic>
+                                Crafted with Precision and Passion.
+                            </Text>
+                            <Text className='pb-3 text-base max-md:hidden' italic>
+                                A World of Exquisite Gemstones
+                            </Text>
+                            <Button type="primary" size='middle' className='bg-[#876553]'>
+                                SHOP NOW
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div>
-            <h3 style={contentStyle}>2</h3>
-        </div>
-        <div>
-            <h3 style={contentStyle}>3</h3>
-        </div>
-        <div>
-            <h3 style={contentStyle}>4</h3>
+            <div className='h-[80vh] relative flex flex-start bg-stone-200'>
+                <img src='/slide3.png' alt="slide3" className='h-full object-contain max-md:object-contain' />
+                <div className='absolute h-full w-full flex justify-center items-center'>
+                    <div className='h-full w-full flex flex-col '>
+                        <div className='h-full w-[90%] flex flex-col justify-center items-center max-md:justify-center max-md:items-end'>
+                            <Title level={4} className='uppercase max-md:hidden'>
+                                Elevate Style with Charm Glow
+                            </Title>
+                            <Text className='pt-3 text-base max-md:hidden' italic>
+                                Crafted with Precision and Passion.
+                            </Text>
+                            <Text className='pb-3 text-base max-md:hidden' italic>
+                                A World of Exquisite Gemstones
+                            </Text>
+                            <Button type="primary" size='middle' className='bg-[#876553]'>
+                                SHOP NOW
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </Carousel>
 );
