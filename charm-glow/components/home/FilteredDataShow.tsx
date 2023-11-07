92491715dd1262e99eb6d85d92a7490e4b029fd3
charmglow <charmglowjwellery@gmail.com>
2023-11-08 00:51:11 +0500

    category filter

--- charm-glow/components/home/FilteredDataShow.tsx
+++ charm-glow/components/home/FilteredDataShow.tsx
@@ -3,23 +3,10 @@ import React from 'react';
 import type { PaginationProps } from 'antd';
 import { Card, Image, List, Pagination, Typography } from 'antd';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
-import { fetchFiterProductsAsync } from '@/store/action/home/homeSlice';
 
 
 const FilteredDataShow = () => {
-    const dispatch = useAppDispatch();
-    const [current, setCurrent] = React.useState(3);
     const { filterProducts } = useAppSelector(state => state.home)
-    const onChange: PaginationProps['onChange'] = (page) => {
-        console.log(page);
-        setCurrent(page);
-    };
-    React.useEffect(() => {
-        dispatch(fetchFiterProductsAsync({
-            category: '',
-            minPrice: 0
-        }))
-    }, [dispatch])
     return (
         <div>
             <List
